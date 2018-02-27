0d63ea125c98577eeb9c3ea59da02eb84c72b82a
Miia-chan <jacek.25@wp.pl>
2018-02-27 09:47:52 +0100

    zmiana

--- warta.php
+++ warta.php
@@ -54,23 +54,25 @@
 
       var target = new Date();
       target.setHours(target.getHours() + 6);
-
+      var now = new Date();
+      var end = new Date();
+      end.setMinutes(0);
+      end.setSeconds(0);
+      while (end.getHours() % 6 != 0) {
+        end.setHours(end.getHours()+1)
+      }
+      console.log(end);
       var x = setInterval(function() {
+        now = new Date();
 
-        var now = new Date().getTime();
-
-
-        var distance = target - now;
-
+        var distance = end - now;
 
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-
         document.getElementById("demo").innerHTML = hours + "h "
         + minutes + "m " + seconds + "s";
-
 
         if (distance < 0) {
             clearInterval(x);
